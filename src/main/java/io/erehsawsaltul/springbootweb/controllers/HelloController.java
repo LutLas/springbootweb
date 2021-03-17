4660d2fd8ba8f5060b7857d7db6cfdcf9fefd88f
Muyoba Lutlas <lutakachihota@gmail.com>
2021-03-17 21:48:20 +0200

    Initial commit

---
+++ src/main/java/io/erehsawsaltul/springbootweb/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package io.erehsawsaltul.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(@RequestParam String name) {
+		return "Hello " + name;
+	}
+}
